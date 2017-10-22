deec3570286de912a42205e19acbbdea0cc4bfe7
AntonKryachko <akruachko@gmail.com>
2017-10-22 19:56:11 +0300

    tz-2 add frame about author and controller

--- src/term_work/translator_assemb_lang/Controller.java
+++
@@ -1,4 +0,0 @@
-package term_work.translator_assemb_lang;
-
-public class Controller {
-}
--- src/term_work/translator_assemb_lang/Main.java
+++ src/term_work/translator_assemb_lang/Main.java
@@ -7,7 +7,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import term_work.translator_assemb_lang.controller.AboutLayerController;
 import term_work.translator_assemb_lang.controller.MainFrameController;
 import term_work.translator_assemb_lang.controller.RootLayerController;
 
@@ -67,5 +69,27 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
+    public void showAboutAuthor(){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("view/AboutLayer.fxml"));
+            AnchorPane pane = loader.load();
 
+            Stage authorStage = new Stage();
+            authorStage.setTitle("About author");
+            authorStage.initModality(Modality.WINDOW_MODAL);
+            authorStage.initOwner(primaryStage);
+            authorStage.setResizable(false);
+
+            Scene scene = new Scene(pane);
+            authorStage.setScene(scene);
+
+            AboutLayerController controller = loader.getController();
+            controller.setAuthorStage(authorStage);
+
+            authorStage.showAndWait();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
