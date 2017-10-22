9bc219aad0674dd481a0e69be66f380d64a88ebf
AntonKryachko <akruachko@gmail.com>
2017-10-22 18:41:56 +0300

    tz-1 add root-layer and main-frame with controllers

---
+++ src/term_work/translator_assemb_lang/Controller.java
@@ -0,0 +1,4 @@
+package term_work.translator_assemb_lang;
+
+public class Controller {
+}
---
+++ src/term_work/translator_assemb_lang/controller/MainFrameController.java
@@ -0,0 +1,14 @@
+package term_work.translator_assemb_lang.controller;
+
+import javafx.fxml.FXML;
+import term_work.translator_assemb_lang.Main;
+
+public class MainFrameController {
+    private Main main;
+    public void setMain(Main main){
+        this.main = main;
+    }
+    public MainFrameController(){}
+    @FXML
+    public void initialize(){}
+}
